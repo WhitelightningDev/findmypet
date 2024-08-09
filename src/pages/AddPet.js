ffcfb10a3b83880bedfb16ae472bdb33016b30e1
WhitelightningDev <danielmommsen@hotmail.com>
2024-08-09 12:54:58 +0200

    "Updated image sizes for tags in AddPet form from 35x35px to 50x50px"

--- src/pages/AddPet.js
+++ src/pages/AddPet.js
@@ -159,7 +159,7 @@ const AddPet = () => {
                     onChange={(e) => setNewPet({ ...newPet, tag: e.target.value })}
                   />
                   <label className="form-check-label d-flex align-items-center" htmlFor="tag1">
-                    <img src={Tagone} alt="Tag 1" className="img-thumbnail me-2" style={{ width: '35px', height: '35px' }} />
+                    <img src={Tagone} alt="Tag 1" className="img-thumbnail me-2" style={{ width: '50px', height: '50px' }} />
                     Tag 1
                   </label>
                 </div>
@@ -174,7 +174,7 @@ const AddPet = () => {
                     onChange={(e) => setNewPet({ ...newPet, tag: e.target.value })}
                   />
                   <label className="form-check-label d-flex align-items-center" htmlFor="tag2">
-                    <img src={Tagtwo} alt="Tag 2" className="img-thumbnail me-2" style={{ width: '35px', height: '35px' }} />
+                    <img src={Tagtwo} alt="Tag 2" className="img-thumbnail me-2" style={{ width: '50px', height: '50px' }} />
                     Tag 2
                   </label>
                 </div>
